0695b8c08a82cdbde027e0a357d9ccb6a394d4bf
marr <overlapjho@yahoo.com>
2021-06-18 06:10:41 +0800

    fix

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -25,7 +25,8 @@ const Dashboard = () => {
 
 	const[ dailyProfits, setdailyProfits] = useState([{"data":[]}])
 	const[ portSnapshot, setportSnapshot] = useState([{"data":[]}])
-	const[ returnSnapshot, setreturnSnapshot] = useState([{"data":[]}])
+	const[ returnPercentSnapshot, setreturnPercentSnapshot] = useState([{"data":[]}])
+	const[ returnDollarSnapshot, setreturnDollarSnapshot] = useState([{"data":[]}])
 
 	const refPortfolio = db.firestore().collection("portfolio")
 	refPortfolio.onSnapshot((snap) => {
@@ -49,20 +50,32 @@ const Dashboard = () => {
 			setportSnapshot(portSnap)
 		})
 	}, []);
+	
 
 	useEffect(() => {
-		const refReturnSnapshot = db.firestore().collection("returns").orderBy('date')
-		refReturnSnapshot.onSnapshot((snap) => {
+		const refReturnPercentSnapshot = db.firestore().collection("returnsPercent").orderBy('date')
+		refReturnPercentSnapshot.onSnapshot((snap) => {
 			let retSnap = [{"data":[]}]
 			snap.forEach((doc) => {
 				retSnap[0].data.push({"x": dayjs(doc.data().date.toDate()).tz().format('MM-D-YYYY H:mm'), "y": doc.data().return })
 			})
-			setreturnSnapshot(retSnap)
+			setreturnPercentSnapshot(retSnap)
 		})
 	}, []);
 
 	useEffect(() => {
-		const refTradesDaily = db.firestore().collection("trades").where('date', '>', new Date(new Date().setHours(0,0,0,0)))
+		const refReturnDollarSnapshot = db.firestore().collection("returnsDollar").orderBy('date')
+		refReturnDollarSnapshot.onSnapshot((snap) => {
+			let retSnap = [{"data":[]}]
+			snap.forEach((doc) => {
+				retSnap[0].data.push({"x": dayjs(doc.data().date.toDate()).tz().format('MM-D-YYYY H:mm'), "y": doc.data().return })
+			})
+			setreturnDollarSnapshot(retSnap)
+		})
+	}, []);
+
+	useEffect(() => {
+		const refTradesDaily = db.firestore().collection("trades").where('date', '>', new Date(new Date().setHours(1,0,0,0))) //adjusted for timezone
 		refTradesDaily.onSnapshot((snap) => {
 			let tempdailyProfits = [{"data":[]}]
 			let pnlSum = 0
@@ -123,7 +136,93 @@ const Dashboard = () => {
 			</Grid>
 			<Grid container spacing={3} ></Grid>
 				<Grid item xs={6}>
-					<h3>Daily PnL</h3>
+					<h3>Daily $ Return </h3>
+					<div style={{ width: '100%', height: '90%' }}>
+						<ResponsiveLine
+							data={returnDollarSnapshot}
+							curve="monotoneX"
+							margin={{ top: 10, right: 0, bottom: 50, left: 50 }}
+							axisTop={null}
+							axisRight={null}
+							lineWidth={1}
+							enableArea={true}
+							enablePoints={false}
+							useMesh={true}
+							colors={{ scheme: 'paired' }}
+							xScale={{ type: "time", format: "%m-%d-%Y %H:%M", useUTC: false, precision: 'day' }}
+							xFormat="time:%m-%d-%Y %H:%M"
+							axisBottom={{
+								orient: 'bottom',
+								format: "%b %d %Y",
+								tickValues: "every 1 day",
+								tickSize: 1,
+								tickPadding: 5,
+								tickRotation: -10,
+								legend: 'Date',
+								legendOffset: 40,
+								legendPosition: 'middle'
+							}}
+							axisLeft={{
+								format: function(value){ 
+									return `$${value}`;
+								},
+								orient: 'left',
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: 0,
+								legend: 'PnL',
+								legendOffset: -40,
+								legendPosition: 'middle'
+							}}
+						/>
+					</div>		
+				</Grid>
+				<Grid item xs={6}>
+					<h3>Daily % Return</h3>
+					<div style={{ width: '100%', height: '90%' }}>
+						<ResponsiveLine
+							data={returnPercentSnapshot}
+							curve="monotoneX"
+							margin={{ top: 10, right: 0, bottom: 50, left: 80 }}
+							yFormat=" >-.2f"
+							axisTop={null}
+							axisRight={null}
+							lineWidth={1}
+							enableArea={false}
+							enablePoints={false}
+							useMesh={true}
+							colors={{ scheme: 'paired' }}
+							xScale={{ type: "time", format: "%m-%d-%Y %H:%M", useUTC: false, precision: 'day' }}
+							xFormat="time:%m-%d-%Y %H:%M"
+							axisBottom={{
+								orient: 'bottom',
+								format: "%b %d %Y",
+								tickValues: "every 1 day",
+								tickSize: 1,
+								tickPadding: 5,
+								tickRotation: -10,
+								legend: 'Date',
+								legendOffset: 40,
+								legendPosition: 'middle'
+							}}
+							axisLeft={{
+								format: function(value){ 
+									return `${value}%`;
+								},
+								orient: 'left',
+								tickSize: 5,
+								tickPadding: 5,
+								tickRotation: 0,
+								legend: 'Percent',
+								legendOffset: -50,
+								legendPosition: 'middle'
+							}}
+						/>
+					</div>
+				</Grid>
+			<Grid container spacing={3} justify="center" >
+				<Grid item xs={6}>
+				<h3>Daily PnL</h3>
 					<div style={{ width: '100%', height: '90%' }}>
 						<ResponsiveLine
 							data={dailyProfits}
@@ -163,55 +262,12 @@ const Dashboard = () => {
 						/>
 					</div>		
 				</Grid>
-				<Grid item xs={6}>
-					<h3>Daily Return</h3>
-					<div style={{ width: '100%', height: '90%' }}>
-						<ResponsiveLine
-							data={returnSnapshot}
-							margin={{ top: 10, right: 0, bottom: 50, left: 80 }}
-							yFormat=" >-.2f"
-							axisTop={null}
-							axisRight={null}
-							lineWidth={1}
-							enableArea={false}
-							enablePoints={false}
-							useMesh={true}
-							colors={{ scheme: 'paired' }}
-							xScale={{ type: "time", format: "%m-%d-%Y %H:%M", useUTC: false, precision: 'day' }}
-							xFormat="time:%m-%d-%Y %H:%M"
-							axisBottom={{
-								orient: 'bottom',
-								format: "%b %d %Y",
-								tickValues: "every 1 day",
-								tickSize: 1,
-								tickPadding: 5,
-								tickRotation: -10,
-								legend: 'Date',
-								legendOffset: 40,
-								legendPosition: 'middle'
-							}}
-							axisLeft={{
-								format: function(value){ 
-									return `${value}%`;
-								},
-								orient: 'left',
-								tickSize: 5,
-								tickPadding: 5,
-								tickRotation: 0,
-								legend: 'Percent',
-								legendOffset: -50,
-								legendPosition: 'middle'
-							}}
-						/>
-					</div>
-				</Grid>
-			<Grid container spacing={3} justify="center" >
-				<Grid item xs={12} >
+				<Grid item xs={6} >
 					<h3>Portfolio Size</h3>
 					<div  style={{ width: '100%', height: '90%' }}>
 						<ResponsiveLine
 							data={portSnapshot}
-							margin={{ top: 10, right: 40, bottom: 90, left: 80 }}
+							margin={{ top: 10, right: 0, bottom: 50, left: 80 }}
 							axisTop={null}
 							axisRight={null}
 							lineWidth={1}
